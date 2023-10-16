83bf0059a18804b770753612ea33c7f5ad3651e7
ram4ik <ram4ik@hotmail.com>
2023-10-16 02:55:48 -0700

    Initial Commit

---
+++ EmptyStates/EmptyStatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyStatesApp.swift
+//  EmptyStates
+//
+//  Created by admin on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct EmptyStatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
